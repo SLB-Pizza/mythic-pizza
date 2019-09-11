7fe6bc35774db44ec2491a44c0b30bb2d22ef5bb
richdacuban <rrjdominguez@gmail.com>
2019-09-11 17:23:01 -0400

    small changes to LandingPage.js

--- src/components/InputPage.js
+++ src/components/InputPage.js
@@ -1,1 +1,3 @@
+//needs to be stateful so the inputs can update state (controlled inputs) and then submit
+
 import React from 'react';
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,24 +7,28 @@ function LandingPage() {
     <div className="landing">
       <div className="intro-text" style={{ whiteSpace: 'pre-line' }}>
         {introText.blurbText.split('\n').map((line, idx) => {
-          return (
-            <div className="intro-text" key={idx}>
-              {line}
-            </div>
-          );
+          return <div key={idx}>{line}</div>;
         })}
         >
       </div>
-      {/* <div> */}
+
       <div classname="vertical-text">{introText.verticalText}</div>
-      {/* </div> */}
-      {/* <div> */}
-      <div className="contact-text">
-        {introText.contactText.map((line, idx) => {
-          return <div key={idx}>{line}</div>;
-        })}
+
+      <div className="right-third">
+        <button className="inquire">INQUIRE</button>
+
+        <div className="contact-text">
+          {introText.contactText.map((line, idx) => {
+            return <div key={idx}>{line}</div>;
+          })}
+        </div>
+
+        <div className="address-text">
+          {introText.addressText.map((line, idx) => {
+            return <div key={idx}>{line}</div>;
+          })}
+        </div>
       </div>
-      {/* </div> */}
     </div>
   );
 }

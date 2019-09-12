6ff309e759ca76dde40fc5f48fb86ec692e8f885
richdacuban <rrjdominguez@gmail.com>
2019-09-11 21:02:48 -0400

    basic class added to InputPage.js

--- src/components/InputPage.js
+++ src/components/InputPage.js
@@ -1,1 +1,23 @@
 import React from 'react';
+import '../App.css';
+
+class InputPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = (event, label) => {
+    this.setState({ label: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return <div>TEST</div>;
+  }
+}
+
+export default InputPage;

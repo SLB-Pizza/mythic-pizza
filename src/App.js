eca2be75816171c72f99c4ea800cb7625d0b0e85
richdacuban <rrjdominguez@gmail.com>
2019-09-20 12:20:16 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Switch, Route, __RouterContext } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 import LandingPage from './components/LandingPage';
 import InputPage from './components/InputPage';
 import SuccessPage from './components/SuccessPage';
 
 function App() {
+  const { location } = useContext(__RouterContext);
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 0, transform: 'translate(100%, 0)' },
+    enter: { opacity: 1, transform: 'translate(0%, 0)' },
+    leave: { opacity: 0, transform: 'translate(-50%, 0)' },
+  });
+
   return (
-    <Router>
+    <>
       <div className="App">
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/form" component={InputPage} />
-        <Route path="/success" component={SuccessPage} />
+        {transitions.map(({ item, props, key }) => (
+          <animated.div key={key} style={props}>
+            <Switch location={item}>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/form" component={InputPage} />
+              <Route exact path="/success" component={SuccessPage} />
+            </Switch>
+          </animated.div>
+        ))}
       </div>
-    </Router>
+    </>
   );
 }
 

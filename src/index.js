72170f2eee649caf219ff3ea5a1487c69d6899d4
Rich Dominguez <RRJDominguez@gmail.com>
2019-10-01 17:50:31 -0400

    removed david font

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./sass/index.scss";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './sass/index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 // FONTS
-import "./fonts/David_temporary_03_Regular.otf";
-import "./fonts/Graphik-Regular.otf";
+// import "./fonts/David_temporary_03_Regular.otf";
+import './fonts/Graphik-Regular.otf';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

c9cefc8d790cffede35901697f10f0e088f7c23b
Christian Mejia <chrismejia10@gmail.com>
2019-10-02 04:13:53 -0400

    Add notFourK to mixins; viewport testing unsucessful
    There's a new place and a picture to explain it - three viewports. The first, is-hidden-desktop will show only for mobile and tablets. The second is the new one; something like is-hidden-touchand4k will display from desktop to quad hd. The final viewport, is-hidden-notfourk, will display the 4k content and styling. Currently, the page is rendering two sets of columns from is-hidden-touch and is-hidden-not4k, even at 4k. There are still issues.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,37 +1,37 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import {
   useSpring,
   /*useTransition,*/ animated,
-  useChain,
+  useChain
   // config,
-} from 'react-spring';
-import { Link } from 'react-router-dom';
-import logo from '../imgs/project-mercury-logo.svg';
-import inquireArrow from '../imgs/inquire-arrow.svg';
-import * as easings from 'd3-ease';
+} from "react-spring";
+import { Link } from "react-router-dom";
+import logo from "../imgs/project-mercury-logo.svg";
+import inquireArrow from "../imgs/inquire-arrow.svg";
+import * as easings from "d3-ease";
 
 function LandingPage() {
   //Left text spring
   const leftText = useSpring({
     config: { duration: 1500, easing: easings.easeCubicOut },
-    from: { opacity: 1, transform: 'translate(0, -400%)' },
-    to: { opacity: 1, transform: 'translate(0, 0)' },
-    delay: 200,
+    from: { opacity: 1, transform: "translate(0, -400%)" },
+    to: { opacity: 1, transform: "translate(0, 0)" },
+    delay: 200
   });
   //Left text spring mobile
   const leftTextMobile = useSpring({
     config: { duration: 1500, easing: easings.easeCubicOut },
-    from: { opacity: 1, transform: 'translate(0, 400%)' },
-    to: { opacity: 1, transform: 'translate(0, 0)' },
-    delay: 200,
+    from: { opacity: 1, transform: "translate(0, 400%)" },
+    to: { opacity: 1, transform: "translate(0, 0)" },
+    delay: 200
   });
   //inquire button move spring
   const inquireMoveRef = useRef();
   const inquireMove = useSpring({
     config: { duration: 1500, easing: easings.easeCubicOut },
-    from: { transform: 'translate(200%, 0)' },
-    to: { transform: 'translate(0, 0)' },
-    ref: inquireMoveRef,
+    from: { transform: "translate(200%, 0)" },
+    to: { transform: "translate(0, 0)" },
+    ref: inquireMoveRef
   });
   //contact info srping
   // const contactInfo = useSpring({
@@ -44,12 +44,12 @@ function LandingPage() {
   const inquireFadeRef = useRef();
   const inquireFade = useSpring({
     config: {
-      duration: 800,
+      duration: 800
     },
     from: { opacity: 0 },
     to: { opacity: 1 },
     // delay: 300,
-    ref: inquireFadeRef,
+    ref: inquireFadeRef
   });
 
   useChain([inquireMoveRef, inquireFadeRef]);
@@ -58,6 +58,66 @@ function LandingPage() {
     <div className="hero is-fullheight" id="landing-bg">
       <div className="hero-body">
         <div className="container">
+          {/*
+              ______                 __ __
+             / ____/___  __  _______/ //_/
+            / /_  / __ \/ / / / ___/ ,<
+           / __/ / /_/ / /_/ / /  / /| |
+          /_/    \____/\__,_/_/  /_/ |_|
+          */}
+          <div className="columns is-hidden-notFourK">
+            <div className="column">
+              {/*
+            // =============================
+            // 1st Column
+            // =============================
+            */}
+              <animated.div style={leftText} id="desktop-blurb">
+                <p>
+                  Attention new thinkers, subversive talents and iconoclasts
+                </p>
+                <p>
+                  Announcing Project Mercury, a joint initiative from Derris and
+                  Mythology (formerly Partners & Spade) to find the undiscovered
+                  founders and radical ideas that will change industries, and
+                  help them build the next great brands.
+                </p>
+              </animated.div>
+            </div>
+            {/*
+            // =============================
+            // 2nd Column
+            // =============================
+            */}
+            <div className="column">
+              <div className="vertical-logo is-flex-desktop">
+                <img
+                  className="vertical-logo-img"
+                  src={logo}
+                  height="65vh"
+                  width="auto"
+                  alt="projectMERCURY"
+                />
+              </div>
+            </div>
+            {/*
+            // =============================
+            // 3rd Column
+            // =============================
+            */}
+            <div className="column">
+              <animated.div style={inquireMove} className="landing-right">
+                <animated.div style={inquireFade} className="btn-animation">
+                  <Link to="/form">
+                    <button className="inquire-btn">
+                      <p>INQUIRE</p>
+                      <img src={inquireArrow} alt="inquire arrow" />
+                    </button>
+                  </Link>
+                </animated.div>
+              </animated.div>
+            </div>
+          </div>
           {/*
               ____            __   __
              / __ \___  _____/ /__/ /_____  ____
@@ -152,7 +212,8 @@ function LandingPage() {
               <animated.div
                 style={inquireMove}
                 className="landing-right"
-                id="mobile-landing">
+                id="mobile-landing"
+              >
                 <animated.div style={inquireFade} className="btn-animation">
                   <Link to="/form">
                     <button className="inquire-mobile">

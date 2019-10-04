f147152f555d78333e98dec7ebcaa6930d3764c7
Christian Mejia <chrismejia10@gmail.com>
2019-10-04 17:46:32 -0400

    Delete test.scss, bulmanotes.txt; remove comments from index.scss, input.scss, landing.scss and TextBox.js

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import '../App.css';
-
-//<p className="insideText">{`${insideText} ${0 + value.length}/500`}</p>
+import React from "react";
+import "../App.css";
 
 export default function TextBox({ onChange, value, name, insideText, mobile }) {
   return (
     <div
       className="text-box"
-      style={{ borderColor: value.length > 0 ? 'white' : 'grey' }}>
-      {/* <div
-        className="text-box-details is-flex"
-        // style={{ color: value.length > 0 ? "white" : "grey" }}
-      > */}
+      style={{ borderColor: value.length > 0 ? "white" : "grey" }}
+    >
       {mobile ? (
         <div className="text-box-details is-flex">
           <div className="text-box-description" id="mobile">
@@ -26,7 +21,6 @@ export default function TextBox({ onChange, value, name, insideText, mobile }) {
           <div className="text-box-char-count">{`${0 + value.length}/500`}</div>
         </div>
       )}
-      {/* </div> */}
 
       <textarea
         name={name}
@@ -34,8 +28,7 @@ export default function TextBox({ onChange, value, name, insideText, mobile }) {
         onChange={onChange}
         maxLength="500"
         rows="6"
-        // required={true}
-        style={{ resize: 'none' }}
+        style={{ resize: "none" }}
       />
     </div>
   );
